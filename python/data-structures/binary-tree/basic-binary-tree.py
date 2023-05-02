48b6f5676bb29d946136c4f3ea15bad22f6f31e2
Adrian Gallano <adriangallanoengr@gmail.com>
2023-05-02 19:17:53 +0800

    Add binary tree

--- python/data-structures/binary-tree/basic-binary-tree.py
+++ python/data-structures/binary-tree/basic-binary-tree.py
@@ -1,9 +1,12 @@
+from collections import deque
 class Node:
     def __init__(self, val = 0):
         self.val = val
-        self.right: Node | None
-        self.left: Node | None
+        self.left = None
+        self.right = None
 
+    def __repr__(self):
+        return f"Node({self.val})"
 
 class BinaryTree:
     def __init__(self, root):
@@ -13,14 +16,57 @@ class BinaryTree:
         if not self.root: return "No tree"
 
         rep = []
-        stack = []
-        curr = self.root
-        while curr or stack: 
-            if curr:
-                stack.append(curr)
-                curr = curr.left
+        queue = deque([self.root])
+        
+        while queue:
+            popped = queue.popleft()
+
+            
+            if not popped:
+                rep.append("x") # represents a non child
+                continue
+
+            rep.append(str(popped.val))
+            if popped.left:
+                queue.append(popped.left)
             else:
-                popped = stack.pop()
-                rep.append(popped)
-                curr = popped.right
+                queue.append(None)
+
+            if popped.right:
+                queue.append(popped.right)
+            else:
+                queue.append(None)
+
+
         return " ".join(rep)
+    
+
+if __name__ == "__main__":
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(5)
+    n6 = Node(6)
+    n7 = Node(7)
+    n8 = Node(8)
+    n9 = Node(9)
+    n10 = Node(10)
+    b1 = BinaryTree(n1)
+
+    n1.right = n2
+    n1.left = n3
+    n2.left = n4
+    n2.right = n5
+    n3.left = n6
+    n3.right = n7
+    n4.left = n8
+    n4.right = n9
+    n5.left = n10
+    print(b1)
+    """ 
+            1
+        3   |   2
+      6   7 | 4    5
+     x x x x 8 9 10
+    """
